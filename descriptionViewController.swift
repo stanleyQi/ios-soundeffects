103d046fcbd175d5aaaf8312b2ee16e6f55c1ef4
qili <qili811227@gmail.com>
2019-09-25 22:01:44 +1200

    Modify typo.

--- descriptionViewController.swift
+++ descriptionViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class descriptionViewController: UIViewController {
 
     var index = 0
-    var descriptions = ["Swithing to turn off the key1 sound.","Swithing to turn off the key2 sound.","Swithing to turn off the key3 sound.","Swithing to turn off the key4 sound.","Swithing to turn off the key5 sound."]
+    var descriptions = ["Switching to turn off the key1 sound.","Switching to turn off the key2 sound.","Switching to turn off the key3 sound.","Switching to turn off the key4 sound.","Switching to turn off the key5 sound."]
 
     @IBOutlet weak var descriptionLable: UITextView!
     
